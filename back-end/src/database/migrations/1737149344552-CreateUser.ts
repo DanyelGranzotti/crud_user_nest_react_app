6ca0a504552d8201f37667dacf34b5c3954a80ff
Danyel Granzotti <danyel.granzotti17@gmail.com>
2025-01-17 18:50:03 -0300

    feat: railway deploy

--- back-end/src/database/migrations/1737149344552-CreateUser.ts
+++ back-end/src/database/migrations/1737149344552-CreateUser.ts
@@ -1,14 +1,18 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUser1737136427964 implements MigrationInterface {
-    name = 'CreateUser1737136427964'
+export class CreateUser1737149344552 implements MigrationInterface {
+    name = 'CreateUser1737149344552'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."user_favoritecolor_enum" AS ENUM('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')`);
+        await queryRunner.query(`CREATE TYPE "public"."user_role_enum" AS ENUM('admin', 'user')`);
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "fullName" character varying NOT NULL, "cpf" character varying NOT NULL, "email" character varying NOT NULL, "favoriteColor" "public"."user_favoritecolor_enum" NOT NULL, "notes" character varying NOT NULL, "role" "public"."user_role_enum" NOT NULL DEFAULT 'user', "password" character varying, CONSTRAINT "UQ_a6235b5ef0939d8deaad755fc87" UNIQUE ("cpf"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TYPE "public"."user_role_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."user_favoritecolor_enum"`);
     }
 
 }
