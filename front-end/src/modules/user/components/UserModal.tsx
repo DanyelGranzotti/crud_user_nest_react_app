676723108005c7dc2f562b37d797adf17bd87636
Danyel Granzotti <danyel.granzotti17@gmail.com>
2025-01-20 03:30:08 -0300

    feat: add reusable Modal component and integrate it into UserModal

--- front-end/src/modules/user/components/UserModal.tsx
+++ front-end/src/modules/user/components/UserModal.tsx
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap";
 import { BsXCircleFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import Modal from "../../../components/common/Modal";
 import { FormTextArea } from "../../../components/form/FormTextArea";
 import { RootState } from "../../../state/rootReducer";
 import { useCreateNote, useGetNotes } from "../../notes/hooks/useNoteHooks";
@@ -93,85 +94,74 @@ const UserModal: React.FC<UserModalProps> = ({ show, onHide, userId }) => {
   if (!isVisible) return null;
 
   return (
-    <div
-      id="user-modal-overlay"
-      className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-500 ${
-        show ? "opacity-100" : "opacity-0"
-      }`}
-    >
-      <div
-        className={`bg-white dark:bg-form-darkgray text-black dark:text-white rounded shadow-lg max-w-md w-full transform transition-transform duration-500 ${
-          show ? "scale-100 translate-y-0" : "scale-75 translate-y-10"
-        }`}
-      >
-        {isLoading && <p>Carregando...</p>}
-        {error && <p>Erro ao carregar informações do usuário.</p>}
-        {user && (
-          <div className="flex justify-between items-center flex-col p-6 gap-6">
-            <div className="flex justify-between items-center w-full">
-              <h2 className="text-base text-modal-title">{user.fullName}</h2>
-              <button
-                onClick={onHide}
-                className="text-card-subtitle hover:opacity-70 transition-opacity duration-300 ease-in-out transform"
-              >
-                <BsXCircleFill size={24} />
-              </button>
-            </div>
-            <div className="flex flex-col gap-2 w-full text-sm">
-              <p className="text-modal-title">
-                <span className="text-card-subtitle">Email:</span> {user.email}
-              </p>
-              <p className="text-modal-title">
-                <span className="text-card-subtitle">CPF:</span> {user.cpf}
-              </p>
-              <p className="text-modal-title">
-                <span className="text-card-subtitle">Cor Favorita:</span>
-                {user.favoriteColor.name}
-              </p>
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <h2 className="text-base text-modal-title">Observações:</h2>
-              <span className="text-xs text-modal-title">
-                {Array.isArray(notes)
-                  ? notes.length > 0
-                    ? notes.slice(0, 2).map((note, index) => (
-                        <div key={index}>
-                          <p>{note.description}</p>
-                          <p className="text-card-subtitle">
-                            Criado em:{" "}
-                            {new Date(note.created_at).toLocaleDateString()}
-                          </p>
-                          {index < notes.length - 1 && <hr className="my-2" />}
-                        </div>
-                      ))
-                    : "Nenhuma observação cadastrada."
-                  : "Erro ao carregar observações."}
-              </span>
-              <FormTextArea
-                controlId="newNote"
-                label="Adicionar Observação"
-                name="newNote"
-                value={newNote}
-                required={true}
-                onChange={(e) => {
-                  setNewNote(e.target.value);
-                  setErrors({ newNote: "" });
-                }}
-                isInvalid={!!errors.newNote}
-                errorMessage={errors.newNote}
-                theme={theme}
-                onKeyPress={handleKeyPress}
-              />
-              <div className="flex justify-end w-full">
-                <Button variant="secondary" onClick={handleAddNote}>
-                  Adicionar Observação
-                </Button>
-              </div>
+    <Modal show={show} onHide={onHide}>
+      {isLoading && <p>Carregando...</p>}
+      {error && <p>Erro ao carregar informações do usuário.</p>}
+      {user && (
+        <div className="flex justify-between items-center flex-col p-6 gap-6">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="text-base text-modal-title">{user.fullName}</h2>
+            <button
+              onClick={onHide}
+              className="text-card-subtitle hover:opacity-70 transition-opacity duration-300 ease-in-out transform"
+            >
+              <BsXCircleFill size={24} />
+            </button>
+          </div>
+          <div className="flex flex-col gap-2 w-full text-sm">
+            <p className="text-modal-title">
+              <span className="text-card-subtitle">Email:</span> {user.email}
+            </p>
+            <p className="text-modal-title">
+              <span className="text-card-subtitle">CPF:</span> {user.cpf}
+            </p>
+            <p className="text-modal-title">
+              <span className="text-card-subtitle">Cor Favorita:</span>
+              {user.favoriteColor.name}
+            </p>
+          </div>
+          <div className="flex w-full flex-col gap-2">
+            <h2 className="text-base text-modal-title">Observações:</h2>
+            <span className="text-xs text-modal-title">
+              {Array.isArray(notes)
+                ? notes.length > 0
+                  ? notes.slice(0, 2).map((note, index) => (
+                      <div key={index}>
+                        <p>{note.description}</p>
+                        <p className="text-card-subtitle">
+                          Criado em:{" "}
+                          {new Date(note.created_at).toLocaleDateString()}
+                        </p>
+                        {index < notes.length - 1 && <hr className="my-2" />}
+                      </div>
+                    ))
+                  : "Nenhuma observação cadastrada."
+                : "Erro ao carregar observações."}
+            </span>
+            <FormTextArea
+              controlId="newNote"
+              label="Adicionar Observação"
+              name="newNote"
+              value={newNote}
+              required={true}
+              onChange={(e) => {
+                setNewNote(e.target.value);
+                setErrors({ newNote: "" });
+              }}
+              isInvalid={!!errors.newNote}
+              errorMessage={errors.newNote}
+              theme={theme}
+              onKeyPress={handleKeyPress}
+            />
+            <div className="flex justify-end w-full">
+              <Button variant="secondary" onClick={handleAddNote}>
+                Adicionar Observação
+              </Button>
             </div>
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </Modal>
   );
 };
 
